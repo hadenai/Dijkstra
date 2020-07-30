8648f71e3bb25a754b33039d90c01a075edecb9b
manoa <manoa@videorurun.com>
2020-07-30 15:59:13 +0200

    Update rubocop for all folder

--- best_way/best_way.rb
+++ best_way/best_way.rb
@@ -1,16 +1,15 @@
 require 'byebug'
 
 class BestWay
-
   POSITION_GLOBAL =
-  [
-    {"position_a" => {"ab" => 8, "ae"=> 8, "af" => 5} },
-    {"position_b" => {"ba" => 8, "bc" => 4, "bf" => 2} },
-    {"position_c" => {"cb" => 4, "cd" => 10, "cf" => 7} },
-    {"position_d" => {"dc" => 10, "de" => 4, "fd" => 8} },
-    {"position_e" => {"ed" => 4, "ea"=> 8} },
-    {"position_f" => {"fa" => 5, "fb" => 2, "fd" => 8, "fc" => 7} }
-  ]
+    [
+      { 'position_a' => { 'ab' => 8, 'ae' => 8, 'af' => 5 } },
+      { 'position_b' => { 'ba' => 8, 'bc' => 4, 'bf' => 2 } },
+      { 'position_c' => { 'cb' => 4, 'cd' => 10, 'cf' => 7 } },
+      { 'position_d' => { 'dc' => 10, 'de' => 4, 'fd' => 8 } },
+      { 'position_e' => { 'ed' => 4, 'ea' => 8 } },
+      { 'position_f' => { 'fa' => 5, 'fb' => 2, 'fd' => 8, 'fc' => 7 } }
+    ].freeze
 
   def initialize(first_position, last_position)
     @first_position = first_position
@@ -18,26 +17,26 @@ class BestWay
   end
 
   def choose_first_points
-    first_points = @first_position.dig(@first_position.keys*"").sort_by { |k, v| [v, k] }.first
+    first_points = @first_position.dig(@first_position.keys * '').min_by { |k, v| [v, k] }
   end
 
   def choose_second_points
     if choose_next_points.count > 1
-    second_position = choose_next_points.select{ |position| position.keys != @first_position.keys }
+      second_position = choose_next_points.select { |position| position.keys != @first_position.keys }
     end
     second_position = choose_next_points
-    two_points = second_position.first.dig(second_position.first.keys*"").sort_by { |k, v| [v, k] }.first
+    two_points = second_position.first.dig(second_position.first.keys * '').min_by { |k, v| [v, k] }
   end
 
   def choose_next_points
     BestWay::POSITION_GLOBAL.select do |position|
       choose_first_point = Hash[*choose_first_points.flatten(1)]
-      position.values.first.has_key?( (choose_first_point.keys*"").reverse)
+      position.values.first.has_key?((choose_first_point.keys * '').reverse)
     end
   end
 
   def add_the_smaller_points
-    @itinerary = (@first_position.keys*"")[-1] + (@last_position.keys*"")[-1]
+    @itinerary = (@first_position.keys * '')[-1] + (@last_position.keys * '')[-1]
     @new_points = {}
     @current_position = choose_first_points.first[0] + choose_second_points.first[1]
     @new_points[@current_position] = choose_first_points.last + choose_second_points.last
@@ -52,6 +51,6 @@ class BestWay
     choose_first_points
     add_the_smaller_points
     return choose_first_points if choose_first_points.first == @itinerary
-    return @new_points if @new_points.keys*"" == @itinerary
+    return @new_points if @new_points.keys * '' == @itinerary
   end
 end
--- spec/best_way/best_way_spec.rb
+++ spec/best_way/best_way_spec.rb
@@ -2,32 +2,31 @@ require 'spec_helper'
 
 RSpec.describe BestWay do
   describe '.choose_itineray' do
-
-    let(:position_a) { {"position_a" => {"ab" => 8, "ad"=> 10, "af" => 5, "ae" => 8 } } }
-    let(:position_b) { {"position_b" => {"ba" => 8, "bc" => 4, "bf" => 2} } }
-    let(:position_c) { {"position_c" => {"cb" => 4, "cd" => 10, "cf" => 7} } }
-    let(:position_d) { {"position_d" => {"dc" => 10, "de" => 4, "fd" => 8, "da" => 10} } }
-    let(:position_e) { {"position_e" => {"ed" => 4, "ea"=> 8} } }
-    let(:position_f) { {"position_f" => {"fa" => 5, "bf" => 2, "fd" => 8, "fc" => 7} } }
+    let(:position_a) { { 'position_a' => { 'ab' => 8, 'ad' => 10, 'af' => 5, 'ae' => 8 } } }
+    let(:position_b) { { 'position_b' => { 'ba' => 8, 'bc' => 4, 'bf' => 2 } } }
+    let(:position_c) { { 'position_c' => { 'cb' => 4, 'cd' => 10, 'cf' => 7 } } }
+    let(:position_d) { { 'position_d' => { 'dc' => 10, 'de' => 4, 'fd' => 8, 'da' => 10 } } }
+    let(:position_e) { { 'position_e' => { 'ed' => 4, 'ea' => 8 } } }
+    let(:position_f) { { 'position_f' => { 'fa' => 5, 'bf' => 2, 'fd' => 8, 'fc' => 7 } } }
 
     context 'when we choose a first_position and last_position different' do
       it 'calculated itinerary a and f' do
-        best_way = BestWay.new(position_a, position_f )
+        best_way = BestWay.new(position_a, position_f)
         output = best_way.choose_itineray
-        expected = ["af", 5]
+        expected = ['af', 5]
         expect(output).to eq(expected)
       end
       it 'calculated itinerary a and b' do
-        best_way = BestWay.new(position_a, position_b )
+        best_way = BestWay.new(position_a, position_b)
         output = best_way.choose_itineray
-        expected = {"ab" => 7}
+        expected = { 'ab' => 7 }
         expect(output).to eq(expected)
       end
     end
     it 'calculated itinerary c and f' do
-      best_way = BestWay.new(position_c, position_f )
+      best_way = BestWay.new(position_c, position_f)
       output = best_way.choose_itineray
-      expected = {"cf" => 6}
+      expected = { 'cf' => 6 }
       expect(output).to eq(expected)
     end
   end
