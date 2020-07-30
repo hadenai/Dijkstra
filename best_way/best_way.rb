b4c932436a3db79b80d96863f402256ed0695a02
manoa <manoa@videorurun.com>
2020-07-30 15:57:27 +0200

    Update spec for all test is green

--- best_way/best_way.rb
+++ best_way/best_way.rb
@@ -1,19 +1,57 @@
 require 'byebug'
 
 class BestWay
-POSITION_A = {"ab" => 8, "ea"=> 8, "af" => 5, "ae" => 10}
-POSITION_B = {"ab" => 8, "bc" => 4, "bf" => 2}
-POSITION_C = {"bc" => 4, "cd" => 10, "de" => 4, "fc" => 7}
-POSITION_D = {"cd" => 10, "de" => 4, "fd" => 8}
-POSITION_E = {"de" => 4, "ea"=> 8, "ae" => 10}
-POSITION_F = {"af" => 5, "bf" => 2, "fd" => 8, "fc" => 7}
-  def initialize(start, finish)
-    @start = start
-    @finish = finish
+
+  POSITION_GLOBAL =
+  [
+    {"position_a" => {"ab" => 8, "ae"=> 8, "af" => 5} },
+    {"position_b" => {"ba" => 8, "bc" => 4, "bf" => 2} },
+    {"position_c" => {"cb" => 4, "cd" => 10, "cf" => 7} },
+    {"position_d" => {"dc" => 10, "de" => 4, "fd" => 8} },
+    {"position_e" => {"ed" => 4, "ea"=> 8} },
+    {"position_f" => {"fa" => 5, "fb" => 2, "fd" => 8, "fc" => 7} }
+  ]
+
+  def initialize(first_position, last_position)
+    @first_position = first_position
+    @last_position = last_position
   end
 
-  def choose_closer_points
-       BestWay::POSITION_A.sort_by { |key, value| value }.first
+  def choose_first_points
+    first_points = @first_position.dig(@first_position.keys*"").sort_by { |k, v| [v, k] }.first
   end
 
+  def choose_second_points
+    if choose_next_points.count > 1
+    second_position = choose_next_points.select{ |position| position.keys != @first_position.keys }
+    end
+    second_position = choose_next_points
+    two_points = second_position.first.dig(second_position.first.keys*"").sort_by { |k, v| [v, k] }.first
+  end
+
+  def choose_next_points
+    BestWay::POSITION_GLOBAL.select do |position|
+      choose_first_point = Hash[*choose_first_points.flatten(1)]
+      position.values.first.has_key?( (choose_first_point.keys*"").reverse)
+    end
+  end
+
+  def add_the_smaller_points
+    @itinerary = (@first_position.keys*"")[-1] + (@last_position.keys*"")[-1]
+    @new_points = {}
+    @current_position = choose_first_points.first[0] + choose_second_points.first[1]
+    @new_points[@current_position] = choose_first_points.last + choose_second_points.last
+    @first_position.values.first.dig(@itinerary)
+    if @new_points.values.first < @first_position.values.first.dig(@itinerary)
+      @first_position.values.first[@itinerary] = @new_points[@current_position]
+    end
+    @new_points
+  end
+
+  def choose_itineray
+    choose_first_points
+    add_the_smaller_points
+    return choose_first_points if choose_first_points.first == @itinerary
+    return @new_points if @new_points.keys*"" == @itinerary
+  end
 end
--- spec/best_way/best_way_spec.rb
+++ spec/best_way/best_way_spec.rb
@@ -1,28 +1,34 @@
 require 'spec_helper'
 
 RSpec.describe BestWay do
-  describe '.calculated' do
-    context 'when we choose a start and finish different' do
+  describe '.choose_itineray' do
+
+    let(:position_a) { {"position_a" => {"ab" => 8, "ad"=> 10, "af" => 5, "ae" => 8 } } }
+    let(:position_b) { {"position_b" => {"ba" => 8, "bc" => 4, "bf" => 2} } }
+    let(:position_c) { {"position_c" => {"cb" => 4, "cd" => 10, "cf" => 7} } }
+    let(:position_d) { {"position_d" => {"dc" => 10, "de" => 4, "fd" => 8, "da" => 10} } }
+    let(:position_e) { {"position_e" => {"ed" => 4, "ea"=> 8} } }
+    let(:position_f) { {"position_f" => {"fa" => 5, "bf" => 2, "fd" => 8, "fc" => 7} } }
+
+    context 'when we choose a first_position and last_position different' do
       it 'calculated itinerary a and f' do
-        best_way = BestWay.new("a", "f")
-        output = best_way.choose_closer_points
+        best_way = BestWay.new(position_a, position_f )
+        output = best_way.choose_itineray
         expected = ["af", 5]
         expect(output).to eq(expected)
       end
-      it 'calculated itinerary b and c' do
+      it 'calculated itinerary a and b' do
+        best_way = BestWay.new(position_a, position_b )
+        output = best_way.choose_itineray
+        expected = {"ab" => 7}
+        expect(output).to eq(expected)
       end
     end
-    context 'when we have an edge case' do
-      context 'when start = a and finish = c' do
-      end
-      context 'when start = a and finish = f' do
-      end
-      context 'when start = b and finish = e' do
-      end
-      context 'when start = e and finish = f' do
-      end
-      context 'when start = f and finish = a' do
-      end
+    it 'calculated itinerary c and f' do
+      best_way = BestWay.new(position_c, position_f )
+      output = best_way.choose_itineray
+      expected = {"cf" => 6}
+      expect(output).to eq(expected)
     end
   end
 end
